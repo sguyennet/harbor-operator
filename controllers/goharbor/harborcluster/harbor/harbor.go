d545f1e5a976b5ac4e95439ab5470142848f3400
wangcanfeng <wangcanfeng@corp.netease.com>
2020-11-11 16:32:17 +0800

    feat(harborcontroller): implement HarborController.
    update getHarborCR method
    
    Signed-off-by: wangcanfeng <wangcanfeng@corp.netease.com>

--- controllers/goharbor/harborcluster/harbor/harbor.go
+++ controllers/goharbor/harborcluster/harbor/harbor.go
@@ -3,7 +3,6 @@ package harbor
 import (
 	"fmt"
 	"github.com/goharbor/harbor-operator/apis/goharbor.io/v1alpha2"
-	"github.com/goharbor/harbor-operator/apis/meta/v1alpha1"
 	"github.com/goharbor/harbor-operator/pkg/k8s"
 	"github.com/goharbor/harbor-operator/pkg/lcm"
 	corev1 "k8s.io/api/core/v1"
@@ -57,7 +56,7 @@ func NewHarborController() lcm.Controller {
 func (harbor *HarborController) getHarborCR(harborcluster *v1alpha2.HarborCluster) *v1alpha2.Harbor {
 	namespacedName := harbor.getHarborCRNamespacedName(harborcluster)
 
-	return &v1alpha2.Harbor{
+	harborCR := &v1alpha2.Harbor{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      namespacedName.Name,
 			Namespace: namespacedName.Namespace,
@@ -68,54 +67,10 @@ func (harbor *HarborController) getHarborCR(harborcluster *v1alpha2.HarborCluste
 				*metav1.NewControllerRef(harborcluster, v1alpha2.HarborClusterGVK),
 			},
 		},
-		Spec: v1alpha2.HarborSpec{
-			HarborComponentsSpec: v1alpha2.HarborComponentsSpec{
-				Portal:      harborcluster.Spec.Portal,
-				Core:        harborcluster.Spec.Core,
-				JobService:  harborcluster.Spec.JobService,
-				Registry:    harborcluster.Spec.Registry,
-				ChartMuseum: harborcluster.Spec.ChartMuseum,
-				Trivy:       harborcluster.Spec.Trivy,
-				Notary:      harborcluster.Spec.Notary,
-				// TODO to filled it
-				Redis: v1alpha2.ExternalRedisSpec{
-					RedisHostSpec: v1alpha1.RedisHostSpec{
-						Host: "",
-						Port: 0,
-					},
-					RedisCredentials: v1alpha1.RedisCredentials{
-						PasswordRef:    "",
-						CertificateRef: "",
-					},
-				},
-				// TODO to filled it
-				Database: v1alpha2.HarborDatabaseSpec{
-					PostgresCredentials: v1alpha1.PostgresCredentials{
-						Username:    "",
-						PasswordRef: "",
-					},
-					Hosts:   nil,
-					SSLMode: "",
-					Prefix:  "",
-				},
-			},
-			Expose:      harborcluster.Spec.Expose,
-			ExternalURL: harborcluster.Spec.ExternalURL,
-			InternalTLS: harborcluster.Spec.InternalTLS,
-			// TODO to filled it
-			ImageChartStorage: v1alpha2.HarborStorageImageChartStorageSpec{
-				Redirect:   v1alpha2.RegistryStorageRedirectSpec{},
-				FileSystem: nil,
-				S3:         nil,
-				Swift:      nil,
-			},
-			LogLevel:               harborcluster.Spec.LogLevel,
-			HarborAdminPasswordRef: harborcluster.Spec.HarborAdminPasswordRef,
-			EncryptionKeyRef:       harborcluster.Spec.EncryptionKeyRef,
-			UpdateStrategyType:     harborcluster.Spec.UpdateStrategyType,
-			Proxy:                  harborcluster.Spec.Proxy,
-		},
+		Spec: *harborcluster.Spec.HarborSpec.DeepCopy(),
 	}
+	// todo to filled with database, cache and storage spec.
+	return harborCR
 }
 
 func harborClusterCRNotReadyStatus(reason, message string) *lcm.CRStatus {
